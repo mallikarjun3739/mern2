f0e59a74f422ba0db4091c587a745bf3026019d4
mallikarjun3739 <mallikarjun16455@gmail.com>
2022-11-12 11:46:57 +0530

    okie

---
+++ src/components/User.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+//const name = "User Functional Component"
+
+//functional react component
+
+function User(){
+    return(
+        <div className="App">
+            <h2>
+                {setInterval(tick, 1000)}
+            </h2>
+        </div>
+    )
+}
+export default User;
+
+function tick(){
+    const element = (
+        <h2>It is {new Date().toDateString()}</h2>,
+        <h2>It is {new Date().toDateString()}</h2>
+    )
+    ReactDOM.render(element,document.getElementById('root'))
+}
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+
+//importing component
+
+import User from "./components/User";
+
+//render
+
+ReactDOM.render(
+<React.StrictMode>
+<User/>
+</React.StrictMode>,
+document.getElementById("root")
+);
